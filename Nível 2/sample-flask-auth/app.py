85d6c836cc0ed03f291b9e450b762824551afa45
Gabriel Trindade (Vivo) <=>
2025-10-29 13:20:18 -0300

    Refactor database initialization and fix user model import errors

--- Nível 2/sample-flask-auth/app.py
+++ Nível 2/sample-flask-auth/app.py
@@ -1,13 +1,14 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "my-secret-key-for-tests"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
+db.init_app(app)
 
-
-db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 
 @app.route("/hello-world", methods=['GET'])
 def hello_world():
