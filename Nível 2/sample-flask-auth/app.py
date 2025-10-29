dcfc272bf52d7348100ef446d88c2fa7e7e33863
Gabriel Trindade (Vivo) <=>
2025-10-29 12:48:16 -0300

    Add initial Flask application setup with basic route

---
+++ Nível 2/sample-flask-auth/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "my-secret-key-for-tests"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://database.db"
+
+
+
+db = SQLAlchemy(app)
+
+@app.route("/hello-world", methods=['GET'])
+def hello_world():
+    return "Hello World"
+
+if  __name__ == '__main__':
+    app.run(debug=True)
