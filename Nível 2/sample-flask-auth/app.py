ad7692b9a3c26d503b4f010567b454cffe18a2d9
Gabriel Trindade <contato.trindadegabriel@gmail.com>
2025-11-03 22:43:00 -0300

    Implement login route with authentication response handling

--- Nível 2/sample-flask-auth/app.py
+++ Nível 2/sample-flask-auth/app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_login import LoginManager
 from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "my-secret-key-for-tests"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+# @login_manager.user_loader
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    try:
+        username = data.get("username")
+        password = data.get("password")
+
+        if username and password:
+            return jsonify({"message": "Usuário autenticado com sucesso!"}), 200
+
+        return jsonify({"message": "Credenciais inválidas!"}), 401 
+    except:
+        return jsonify({"message": "Erro ao processar login!"}), 500
+        
 
 with app.app_context():
     db.create_all()
